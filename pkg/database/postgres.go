0902fa9dbdb3bec17632096dbeac647e92a84fa8
GorevA <artgor005@gmail.com>
2025-04-20 01:03:44 +0800

    ---

--- pkg/database/postgres.go
+++ pkg/database/postgres.go
@@ -2,18 +2,32 @@ package database
 
 import (
 	"fmt"
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
 	"log"
 	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 func ConnectDB() *gorm.DB {
-	dsn := os.Getenv("DATABASE_DSN")
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+
+	if host == "" || port == "" || user == "" || dbname == "" {
+		log.Fatalf("Missing one or more required DB environment variables")
+	}
+
+	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
-		log.Fatal("Failed to connect to database: ", err)
+		log.Fatalf("Failed to connect to database: %v", err)
 	}
-	fmt.Println("Database connection established")
+
+	log.Println("Database connection established")
 	return db
 }
