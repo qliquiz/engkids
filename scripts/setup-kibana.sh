866fdeef5f62212d18d872a50a97ff2679312874
GorevA <artgor005@gmail.com>
2025-04-21 12:24:22 +0800

    -

--- scripts/setup-kibana.sh
+++ scripts/setup-kibana.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-echo "Waiting for Kibana to start..."
-while ! curl -s http://kibana:5601/api/status | grep -q '"status":{"overall":{"level":"available"'; do
-  sleep 5
-done
+EXISTS=$(curl -s -o /dev/null -w "%{http_code}" http://kibana:5601/api/saved_objects/index-pattern/engkids-logs)
 
-echo "Kibana is up. Setting up index patterns..."
-
-curl -X POST "http://kibana:5601/api/saved_objects/index-pattern/engkids-logs" \
-  -H "kbn-xsrf: true" \
-  -H "Content-Type: application/json" \
-  -d'{"attributes":{"title":"engkids-logs-*","timeFieldName":"@timestamp"}}'
-
-echo "Index pattern created!"
+if [ "$EXISTS" -ne 200 ]; then
+  echo "Creating index pattern..."
+  curl -X POST "http://kibana:5601/api/saved_objects/index-pattern/engkids-logs" \
+    -H 'kbn-xsrf: true' \
+    -H 'Content-Type: application/json' \
+    -d '{
+      "attributes": {
+        "title": "engkids-logs",
+        "timeFieldName": "@timestamp"
+      }
+    }'
+else
+  echo "Index pattern already exists, skipping"
+fi
